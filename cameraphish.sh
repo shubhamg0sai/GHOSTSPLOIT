9c7968261e91312e7945c0b8c909e98e7644d550
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-05 07:00:50 +0530

    Add files via upload

---
+++ cameraphish.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by dheeraj now start"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+echo   " we are anynomous"               
+ echo   " we are hackers ok "                                                                    
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt-get update -y                      #update in progress
+apt install python -y           #installation......
+apt instll git -y
+apt install python -y
+apt install python2 -y
+apt install php -y
+apt install openssh -y
+git clone https://github.com/techchipnet/CamPhish
+cd CamPhish
+ls
+bash camphish.sh
+echo  "#####################################"
+echo   " u can now hack anything      "               
+ echo   " completing wait 3 2 1 "                                                                    
+echo   "####################################"
+
+
+echo  "#####################################"
+echo   "anynomous "               
+ echo   " join us on facebook Dhee Techno Hacker "                                                                   echo   "####################################"
+
+
+echo " "                                                                   
+echo   "####################################"
+
+                                                            echo   " "
+                                                            
+echo  "#####################################"
+echo   " we are hackers "               
+                                                                  echo   "####################################"
+
+echo  "#####################################"
+               
+echo   " processing wait......
+"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then u downloaded success fully subscribe us Dhee Techno Hacker channel ...we are learners dont underestimate us "
+
+echo " ??? now rock and hack ??? "
+
+exit
