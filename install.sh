b52e779f7c27f3e97f4b2dd100156279751d0310
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-12-26 13:16:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,65 @@
+
+
+
+                                           IyEvY
+                                        mluL2Jhc2gK
+                                    ZWNobyAkcmVkIAogZWN
+                              obyAiIi0tLS0tLS0tLS0tLS0tLS0tL
+                          S0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0
+                      tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tICIiC
+                  mVjaG8gIiBUaGlzIFNjcmlwdCBpcyBTaHVCaGFtZzBzYWluIGJ5IG
+              5vdyBzdGFydCIKZWNobyAiLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS
+          0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tL
+       S0tIgoKZWNobyAiMDA3IgplY2hvICIwMDciCmVjaG8gIjAwNyIKZWNobyAiLiIKCmVjaG8gIC
+    IjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgYW55b
+  m9tb3VzIiAgICAgICAgICAgICAgIAogZWNobyAgICIgd2UgYXJlIGhhY2tlcnMgb2sgIiAgICAgICAgICAgI
+CAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICAiIy
+  MjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAiMDA3IgplY2hvICIwMDciCmVjaG
+    8gIjAwNyIKCmVjaG8gIi4iCgphcHQtZ2V0IHVwZGF0ZSAteSAgICAgICAgICAgICAgICAgICAgICAjd
+       XBkYXRlIGluIHByb2dyZXNzCmFwdCBpbnN0YWxsIHB5dGhvbiAteSAgICAgICAgICAgI2luc3
+          RhbGxhdGlvbi4uLi4uLgphcHQgaW5zdGxsIGdpdCAteQphcHQgaW5zdGFsbCBweXRob
+              24gLXkKYXB0IGluc3RhbGwgcHl0aG9uMiAteQphcHQgaW5zdGFsbCBwaHAgL
+                  XkKYXB0IGluc3RhbGwgb3BlbnNzaCAteQpnaXQgY2xvbmUgaHR0cH
+                      M6Ly9naXRodWIuY29tL01hc3RlckRldlgvVGVybXV4LUFE
+                      QgpjZCBUZXJtdXgtQURCCmJhc2ggSW5zdGFsbFRvb2xzLn
+                      NoCmxzCmNkCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5j
+                      b20vU2h1QmhhbWcwc2Fpbi9waG9uZWhhY2sKY2QgcGhvbm
+                      VoYWNrCmxzCmNkIGluc3RhbGwKbHMKcHl0aG9uMiBtYWlu
+                      X2xpbnV4LnB5CnkKZWNobyAgIiMjIyMjIyMjIyMjIyMjIy
+                      MjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCmVjaG8gICAiIHUg
+                      Y2FuIG5vdyBoYWNrIGFueXRoaW5nICAgICAgIiAgICAgIC
+                      AgICAgICAgIAogZWNobyAgICIgY29tcGxldGluZyB3YWl0
+                      IDMgMiAxICIgICAgICAgICAgICAgICAgICAgICAgICAgIC
+                      AgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+                      ICAgICAgIAplY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIy
+                      MjIyMjIyMjIyMjIyMjIyMjIyIKCgplY2hvICAiIyMjIyMj
+                      IyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKZW
+                      NobyAgICJhbnlub21vdXMgIiAgICAgICAgICAgICAgIAog
+                 ZWNobyAgICIgam9pbiB1cyBvbiBmYWNlYm9vayBodHRwczovL20uZmFj
+               ZWJvb2suY29tL3NodWJoYW0uZ29zYWluLjk4MCAiICAgICAgICAgICAgICAg
+                 ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+               ICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj
+                 IyMjIyMjIyMjIgoKCmVjaG8gIiAiICAgICAgICAgICAgICAgICAgICAg
+               ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+                 IAplY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj
+IyMjIyIKCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGV
+jaG8gICAiICIKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC
+AgCmVjaG8gICIjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgaGFja
+2VycyAiICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWN
+obyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCiAgICAgICAgICAgICAgIAplY2hvICAgIi
+Bwcm9jZXNzaW5nIHdhaXQuLi4uLi4KIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgI
+CAgICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj
+IgoKZWNobyAiIElmIFlvdSBDb25uZWN0ZWQgVG8gSW50ZXJuZXQgVGhlbiB1IGRvd25sb2FkZWQgc3VjY2VzcyBmdWx
+seSBmb2xsb3cgbWUgSW5zdGFncmFtIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHNhaW4vP2hsPW
+             VuICIKCmVjaG8gIiA/Pz8gbm93IHJvY2sgYW5kIGhhY2sgPz8/ICIKCmV4aXQK
+
+
+
+
+
+
+
 #!/bin/bash
 echo $red 
  echo ""----------------------------------------------------------------------------- ""
