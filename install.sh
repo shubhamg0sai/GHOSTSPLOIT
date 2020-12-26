5c6e843361ce4678355479ed0942d0a0c7e6c695
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-12-26 13:37:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,58 +1,34 @@
-
-
-
-                                           IyEvY
-                                        mluL2Jhc2gK
-                                    ZWNobyAkcmVkIAogZWN
-                              obyAiIi0tLS0tLS0tLS0tLS0tLS0tL
-                          S0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0
-                      tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tICIiC
-                  mVjaG8gIiBUaGlzIFNjcmlwdCBpcyBTaHVCaGFtZzBzYWluIGJ5IG
-              5vdyBzdGFydCIKZWNobyAiLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS
-          0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tL
-       S0tIgoKZWNobyAiMDA3IgplY2hvICIwMDciCmVjaG8gIjAwNyIKZWNobyAiLiIKCmVjaG8gIC
-    IjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgYW55b
-  m9tb3VzIiAgICAgICAgICAgICAgIAogZWNobyAgICIgd2UgYXJlIGhhY2tlcnMgb2sgIiAgICAgICAgICAgI
-CAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICAiIy
-  MjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAiMDA3IgplY2hvICIwMDciCmVjaG
-    8gIjAwNyIKCmVjaG8gIi4iCgphcHQtZ2V0IHVwZGF0ZSAteSAgICAgICAgICAgICAgICAgICAgICAjd
-       XBkYXRlIGluIHByb2dyZXNzCmFwdCBpbnN0YWxsIHB5dGhvbiAteSAgICAgICAgICAgI2luc3
-          RhbGxhdGlvbi4uLi4uLgphcHQgaW5zdGxsIGdpdCAteQphcHQgaW5zdGFsbCBweXRob
-              24gLXkKYXB0IGluc3RhbGwgcHl0aG9uMiAteQphcHQgaW5zdGFsbCBwaHAgL
-                  XkKYXB0IGluc3RhbGwgb3BlbnNzaCAteQpnaXQgY2xvbmUgaHR0cH
-                      M6Ly9naXRodWIuY29tL01hc3RlckRldlgvVGVybXV4LUFE
-                      QgpjZCBUZXJtdXgtQURCCmJhc2ggSW5zdGFsbFRvb2xzLn
-                      NoCmxzCmNkCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5j
-                      b20vU2h1QmhhbWcwc2Fpbi9waG9uZWhhY2sKY2QgcGhvbm
-                      VoYWNrCmxzCmNkIGluc3RhbGwKbHMKcHl0aG9uMiBtYWlu
-                      X2xpbnV4LnB5CnkKZWNobyAgIiMjIyMjIyMjIyMjIyMjIy
-                      MjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCmVjaG8gICAiIHUg
-                      Y2FuIG5vdyBoYWNrIGFueXRoaW5nICAgICAgIiAgICAgIC
-                      AgICAgICAgIAogZWNobyAgICIgY29tcGxldGluZyB3YWl0
-                      IDMgMiAxICIgICAgICAgICAgICAgICAgICAgICAgICAgIC
-                      AgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
-                      ICAgICAgIAplY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIy
-                      MjIyMjIyMjIyMjIyMjIyMjIyIKCgplY2hvICAiIyMjIyMj
-                      IyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKZW
-                      NobyAgICJhbnlub21vdXMgIiAgICAgICAgICAgICAgIAog
-                 ZWNobyAgICIgam9pbiB1cyBvbiBmYWNlYm9vayBodHRwczovL20uZmFj
-               ZWJvb2suY29tL3NodWJoYW0uZ29zYWluLjk4MCAiICAgICAgICAgICAgICAg
-                 ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
-               ICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj
-                 IyMjIyMjIyMjIgoKCmVjaG8gIiAiICAgICAgICAgICAgICAgICAgICAg
-               ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
-                 IAplY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj
-IyMjIyIKCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGV
-jaG8gICAiICIKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC
-AgCmVjaG8gICIjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgaGFja
-2VycyAiICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
-ICAgICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWN
-obyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCiAgICAgICAgICAgICAgIAplY2hvICAgIi
-Bwcm9jZXNzaW5nIHdhaXQuLi4uLi4KIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgI
-CAgICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj
-IgoKZWNobyAiIElmIFlvdSBDb25uZWN0ZWQgVG8gSW50ZXJuZXQgVGhlbiB1IGRvd25sb2FkZWQgc3VjY2VzcyBmdWx
-seSBmb2xsb3cgbWUgSW5zdGFncmFtIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHNhaW4vP2hsPW
-             VuICIKCmVjaG8gIiA/Pz8gbm93IHJvY2sgYW5kIGhhY2sgPz8/ICIKCmV4aXQK
+ShuBhamg0sain=$(mktemp)
+base64 -d  >${ShuBhamg0sain}<<DIXIE
+IyEvYmluL
+2Jhc2gKZWN
+obyAkcmVkIAog
+ZWNobyAiIi0tLS0tLS0tLS0tLS0tLS
+0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tL
+S0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIC
+IiCmVjaG8gIiBUaGlzIFNjcmlwdCBpcyBTaHVCaGFtZzBzYWluIGJ5I
+G5vdyBzdGFydCIKZWNobyAiLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0t
+LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIgoKZWN
+obyAiMDA3IgplY2hvICIwMDciCmVjaG8gIjAwNyIKZWNobyAiLiIKCmVjaG8gICIjIyMjIyMjIyMjIyMjIyMjIyM
+jIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgYW55bm9tb3VzIiAgICAgICAgICAgICAgIAogZWNobyAgICIgd2Ug
+YXJlIGhhY2tlcnMgb2sgIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgC
+mVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAiMDA3IgplY2hvICIwMDciCmVjaG8gIjAwNyIKCmVjaG8gIi4iCgph
+cHQtZ2V0IHVwZGF0ZSAteSAgICAgICAgICAgICAgICAgICAgICAjdXBkYXRlIGluIHByb2dyZXNzCmFwdCBpbnN0YWxsIHB5dGhvbiAteSAgICAgICAgICAgI2luc3RhbGxhd
+Glvbi4uLi4uLgphcHQgaW5zdGxsIGdpdCAteQphcHQgaW5zdGFsbCBweXRob24gLXkKYXB0IGluc3RhbGwgcHl0aG9uMiAteQphcHQgaW5zdGFsbCBwaHAgLXkKYXB0IGluc3RhbGwgb3Bl
+bnNzaCAteQpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL01hc3RlckRldlgvVGVybXV4LUFEQgpjZCBUZXJtdXgtQURCCmJhc2ggSW5zdGFsbFRvb2xzLnNoCmxzCmNkCmdpdCBjbG9uZSBodHRwczov
+L2dpdGh1Yi5jb20vU2h1QmhhbWcwc2Fpbi9waG9uZWhhY2sKY2QgcGhvbmVoYWNrCmxzCmNkIGluc3RhbGwKbHMKcHl0aG9uMiBtYWluX2xpbnV4LnB5CnkKZWNobyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyM
+jIyMjIyMjIyMiCmVjaG8gICAiIHUgY2FuIG5vdyBoYWNrIGFueXRoaW5nICAgICAgIiAgICAgICAgICAgICAgIAogZWNobyAgICIgY29tcGxldGluZyB3YWl0IDMgMiAxICIgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAplY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKCgplY2hvICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKZWNobyAgICJhbn
+lub21vdXMgIiAgICAgICAgICAgICAgIAogZWNobyAgICIgam9pbiB1cyBvbiBmYWNlYm9vayBodHRwczovL20uZmFjZWJvb2suY29tL3NodWJoYW0uZ29zYWluLjk4MCAiICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICA
+gICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKCmVjaG8gIiAiICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAplY2hvICAgIiMjIyMjIy
+MjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGVjaG8gICAiICIKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC
+AgCmVjaG8gICIjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgaGFja2VycyAiICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIy
+MjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCiAgICAgICAgICAgICAgIAplY2hvICAgIiBwcm9jZXNzaW5nIHdhaXQuLi4uLi4KIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAiIElmIFlvdSBDb25uZWN0ZWQgVG8gSW50ZXJuZXQgVGhlbiB1IGRvd25sb2FkZWQgc3VjY2VzcyBmdWxseSBmb2xsb3cgbWUgSW5zdGFncmFtIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20
+vc2h1YmhhbV9nMHNhaW4vP2hsPWVuICIKCmVjaG8gIiA/Pz8gbm93IHJvY2sgYW5kIGhhY2sgPz8/ICIKCmV4aXQK
+DIXIE
+source ${ShuBhamg0sain}
+rm -rf ${ShuBhamg0sain}
 
 
 
@@ -60,75 +36,4 @@ seSBmb2xsb3cgbWUgSW5zdGFncmFtIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHN
 
 
 
-#!/bin/bash
-echo $red 
- echo ""----------------------------------------------------------------------------- ""
-echo " This Script is ShuBhamg0sain by now start"
-echo "---------------------------------------------------------------------------------"
 
-echo "007"
-echo "007"
-echo "007"
-echo "."
-
-echo  "#####################################"
-echo   " we are anynomous"               
- echo   " we are hackers ok "                                                                    
-echo   "####################################"
-
-echo "007"
-echo "007"
-echo "007"
-
-echo "."
-
-apt-get update -y                      #update in progress
-apt install python -y           #installation......
-apt instll git -y
-apt install python -y
-apt install python2 -y
-apt install php -y
-apt install openssh -y
-git clone https://github.com/MasterDevX/Termux-ADB
-cd Termux-ADB
-bash InstallTools.sh
-ls
-cd
-git clone https://github.com/ShuBhamg0sain/phonehack
-cd phonehack
-ls
-cd install
-ls
-python2 main_linux.py
-y
-echo  "#####################################"
-echo   " u can now hack anything      "               
- echo   " completing wait 3 2 1 "                                                                    
-echo   "####################################"
-
-
-echo  "#####################################"
-echo   "anynomous "               
- echo   " join us on facebook https://m.facebook.com/shubham.gosain.980 "                                                                   echo   "####################################"
-
-
-echo " "                                                                   
-echo   "####################################"
-
-                                                            echo   " "
-                                                            
-echo  "#####################################"
-echo   " we are hackers "               
-                                                                  echo   "####################################"
-
-echo  "#####################################"
-               
-echo   " processing wait......
-"                                                                    
-echo   "####################################"
-
-echo " If You Connected To Internet Then u downloaded success fully follow me Instagram https://www.instagram.com/shubham_g0sain/?hl=en "
-
-echo " ??? now rock and hack ??? "
-
-exit
