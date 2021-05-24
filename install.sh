99b8a27c325d5cfe84f15a6ef6e88a4a71528832
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-24 15:28:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,119 +1,50 @@
 ShuBhamg0sain=$(mktemp)
 base64 -d  >${ShuBhamg0sain}<<DIXIE
-IyEvYmluL
-2Jhc2gKZWN
-obyAkcmVkIAog
-ZWNobyAiIi0tLS0tLS0tLS0tLS0tLS
-0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tL
-S0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIC
-IiCmVjaG8gIiBUaGlzIFNjcmlwdCBpcyBTaHVCaGFtZzBzYWluIGJ5I
-G5vdyBzdGFydCIKZWNobyAiLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0t
-LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIgoKZWN
-obyAiMDA3IgplY2hvICIwMDciCmVjaG8gIjAwNyIKZWNobyAiLiIKCmVjaG8gICIjIyMjIyMjIyMjIyMjIyMjIyM
-jIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgYW55bm9tb3VzIiAgICAgICAgICAgICAgIAogZWNobyAgICIgd2Ug
-YXJlIGhhY2tlcnMgb2sgIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgC
-mVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAiMDA3IgplY2hvICIwMDciCmVjaG8gIjAwNyIKCmVjaG8gIi4iCgph
-cHQtZ2V0IHVwZGF0ZSAteSAgICAgICAgICAgICAgICAgICAgICAjdXBkYXRlIGluIHByb2dyZXNzCmFwdCBpbnN0YWxsIHB5dGhvbiAteSAgICAgICAgICAgI2luc3RhbGxhd
-Glvbi4uLi4uLgphcHQgaW5zdGxsIGdpdCAteQphcHQgaW5zdGFsbCBweXRob24gLXkKYXB0IGluc3RhbGwgcHl0aG9uMiAteQphcHQgaW5zdGFsbCBwaHAgLXkKYXB0IGluc3RhbGwgb3Bl
-bnNzaCAteQpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL01hc3RlckRldlgvVGVybXV4LUFEQgpjZCBUZXJtdXgtQURCCmJhc2ggSW5zdGFsbFRvb2xzLnNoCmxzCmNkCmdpdCBjbG9uZSBodHRwczov
-L2dpdGh1Yi5jb20vU2h1QmhhbWcwc2Fpbi9waG9uZWhhY2sKY2QgcGhvbmVoYWNrCmxzCmNkIGluc3RhbGwKbHMKcHl0aG9uMiBtYWluX2xpbnV4LnB5CnkKZWNobyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyM
-jIyMjIyMjIyMiCmVjaG8gICAiIHUgY2FuIG5vdyBoYWNrIGFueXRoaW5nICAgICAgIiAgICAgICAgICAgICAgIAogZWNobyAgICIgY29tcGxldGluZyB3YWl0IDMgMiAxICIgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
-ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAplY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKCgplY2hvICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKZWNobyAgICJhbn
-lub21vdXMgIiAgICAgICAgICAgICAgIAogZWNobyAgICIgam9pbiB1cyBvbiBmYWNlYm9vayBodHRwczovL20uZmFjZWJvb2suY29tL3NodWJoYW0uZ29zYWluLjk4MCAiICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICA
-gICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKCmVjaG8gIiAiICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAplY2hvICAgIiMjIyMjIy
-MjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGVjaG8gICAiICIKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC
-AgCmVjaG8gICIjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgaGFja2VycyAiICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIy
-MjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCiAgICAgICAgICAgICAgIAplY2hvICAgIiBwcm9jZXNzaW5nIHdhaXQuLi4uLi4KIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
-ICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAiIElmIFlvdSBDb25uZWN0ZWQgVG8gSW50ZXJuZXQgVGhlbiB1IGRvd25sb2FkZWQgc3VjY2VzcyBmdWxseSBmb2xsb3cgbWUgSW5zdGFncmFtIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20
-vc2h1YmhhbV9nMHNhaW4vP2hsPWVuICIKCmVjaG8gIiA/Pz8gbm93IHJvY2sgYW5kIGhhY2sgPz8/ICIKCmV4aXQK
+IyEvYm
+luL2Jhc2gKZWN
+obyAkcmVkIAogZWNobyA
+iIi0tLS0tLS0tLS0tLS0tLS0tLS
+0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0t
+LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tL
+S0tLS0tLS0tICIiCmVjaG8gIiBUaGlzIFNjcm
+lwdCBpcyBTaHVCaGFtZzBzYWluIGJ5IG5vd
+yBzdGFydCIKZWNobyAiLS0tLS0tLS0tLS0
+tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0
+tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0
+tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIgoKZW
+NobyAiMDA3IgplY2hvICIwMDciCmVjaG
+8gIjAwNyIKZWNobyAiLiIKCmVjaG8gICI
+jIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIy
+MjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUg
+YW55bm9tb3VzIiAgICAgICAgICAgICAgIAog
+ZWNobyAgICIgd2UgYXJlIGhhY2tlcnMgb2sgIiAgI
+CAgICAgICAgICAgICAgICAgIC
+AgICAgICAgICAgICAgICAgICAg
+ICAgICAgICAgICAgICAgICAgICAgIC
+AgICAgCmVjaG8gICAiIyMjIyMjIyMjIyMj
+IyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIg
+oKZWNobyAiMDA3IgplY2hvICIwMDciCmVja
+G8gIjAwNyIKCmVjaG8gIi4iCgphcHQtZ2V0I
+HVwZGF0ZSAteQpjbGVhciAgICAgICAgICAgICAgICAgICA
+gICAjdXBkYXRlIGluIHByb2dyZXNzCmFwdCBpbnN0YWxsIHB5dGh
+vbiAteSAgCmNsZWFyICAgICAgICAgI2luc3RhbGxhdGlvbi4uLi4uLgphcHQgaW5zdGxsIGdpdCAteQ
+pjbGVhcgphcHQgaW5zdGFsbCBweXRob24gLXkKY2xlYXIKYXB0IGluc3RhbGwgcHl0aG9uMiAteQpjbGVhcgphcHQgaW5zdGFsbCBwaHAgLXkKY2xlYXIKYXB0IGluc3RhbGwgb3Blbn
+NzaCAteQpjbGVhcgpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL01hc3RlckRldlgvVGVybXV4LUFEQgpjZCBUZXJtdXgtQURCCmJhc2ggSW5zdGFsbFRvb2xzLnNoCmxzCmNkCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vU2h1QmhhbWcwc2Fpbi9wa
+G9uZWhhY2sKY2QgcGhvbmVoYWNrCmxzCmNkIGluc3RhbGwKbHMKcHl0aG9uMiBtYWluX2xpbnV4LnB5CmNsZWFyCnkKZWNobyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCmVjaG8gICAiIHUgY2FuIG5vdyBoYWNrIGFueXRoaW5nICAgICAgIiAgICAgICAgICAgICAgIAogZWNobyAgICIgY29tcGxldGluZyB3YWl0
+IDMgMiAxICIgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAplY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKCgplY2hvICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKZWNobyAgICJhbnlub21vdXMgIiAgICAgICAgICAgICAgIAogZWNobyAgICIgam9pbiB1cyBvbiBmYWNl
+Ym9vayBodHRwczovL20uZmFjZWJvb2suY29tL3NodWJoYW0uZ29zYWluLjk4MCAiICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKCmVjaG8gIiAiICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAp
+lY2hvICAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyIKCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGVjaG8gICAiICIKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICIjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgplY2hvICAgIiB3ZSBhcmUgaGFja2VycyAiICAgICAgICAgICAgI
+CAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGVjaG8gICAiIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIgoKZWNobyAgIiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMiCiAgICAgICAgICAgICAgIAplY2hvICAgIiBwcm9jZXNzaW5nIHdhaXQuLi4uLi4KIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCmVjaG8gICA
+iIyMjIyMjIyMjIyMjIyMjIyMjI
+yMjIyMjIyMjIyMjIyMjIyMjIgoKZWNob
+yAiIElmIFlvdSBDb25uZWN0ZWQgVG8gSW50ZXJ
+uZXQgVGhlbiB1IGRvd25sb
+2FkZWQgc3VjY2VzcyBmdW
+xseSBmb2xsb3cgbWUgSW5zdGFncm
+FtIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS
+5jb20vc2h1YmhhbV9nMHNhaW4vP2hsPWVuICIKCmV
+jaG8gIiA/Pz8gbm93IHJvY2sgYW5kIGh
+hY2sgPz8/ICIKCmV4aXQKCgoKCg==
 DIXIE
 source ${ShuBhamg0sain}
 rm -rf ${ShuBhamg0sain}
-
-
-
-
-#!/bin/bash
-echo $red 
- echo ""----------------------------------------------------------------------------- ""
-echo " This Script is ShuBhamg0sain by now start"
-echo "---------------------------------------------------------------------------------"
-
-echo "007"
-echo "007"
-echo "007"
-echo "."
-
-echo  "#####################################"
-echo   " we are anynomous"               
- echo   " we are hackers ok "                                                                    
-echo   "####################################"
-
-echo "007"
-echo "007"
-echo "007"
-
-echo "."
-
-apt-get update -y
-clear                      #update in progress
-apt install python -y  
-clear         #installation......
-apt instll git -y
-clear
-apt install python -y
-clear
-apt install python2 -y
-clear
-apt install php -y
-clear
-apt install openssh -y
-clear
-git clone https://github.com/MasterDevX/Termux-ADB
-cd Termux-ADB
-bash InstallTools.sh
-ls
-cd
-git clone https://github.com/ShuBhamg0sain/phonehack
-cd phonehack
-ls
-cd install
-ls
-python2 main_linux.py
-clear
-y
-echo  "#####################################"
-echo   " u can now hack anything      "               
- echo   " completing wait 3 2 1 "                                                                    
-echo   "####################################"
-
-
-echo  "#####################################"
-echo   "anynomous "               
- echo   " join us on facebook https://m.facebook.com/shubham.gosain.980 "                                                                   echo   "####################################"
-
-
-echo " "                                                                   
-echo   "####################################"
-
-                                                            echo   " "
-                                                            
-echo  "#####################################"
-echo   " we are hackers "               
-                                                                  echo   "####################################"
-
-echo  "#####################################"
-               
-echo   " processing wait......
-"                                                                    
-echo   "####################################"
-
-echo " If You Connected To Internet Then u downloaded success fully follow me Instagram https://www.instagram.com/shubham_g0sain/?hl=en "
-
-echo " ??? now rock and hack ??? "
-
-exit
-
-
-
-
