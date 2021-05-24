8b89cdb81700730c5619edda345103a287d6919f
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-24 21:08:25 +0530

    Create logo.sh

---
+++ install/logo.sh
@@ -0,0 +1,101 @@
+logo_design_1 = Fore.RED + '''
+◈•─────•◈•@ShuBhamg0sain•◈•─────•◈•
+      $    $     $     $    $
+     $    $$     $     $$    $
+    $$   $$      $      $$    $$
+    $$   $$      $      $$    $
+   $$    $$      $      $$    $$
+   $$    $$    $$$$$    $$    $$
+   $$   $$$   $$$$$$$   $$$   $$
+  $$$   $$$   $$$$$$$   $$$   $$$
+  $$$   $$$   $$$$$$$   $$$   $$$
+  $$$   $$$    $$$$$    $$$   $$$
+  $$$    $$$   $$$$$   $$$   $$$$
+   $$$$$$$$-GHOSTSPLOIT-$$$$$$$$
+           $$$$$$$$$$$$$$
+            $$$$$$$$$$$$
+   $$$$$$$$-GHOSTSPLOIT-$$$$$$$$
+  $$$$$$$$$$_$$$$$ $$$$$-$$$$$$$$$$
+ $$$$   $$$  $$$$   $$$$  $$$   $$$$
+ $$$    $$$  $$$$$ $$$$$  $$$    $$$
+  $$$   $$$  $$$     $$$  $$$   $$$
+  $$$   $$$  $$$$$ $$$$$  $$$   $$$
+   $$    $$   $$$   $$$   $$    $$
+   $$$   $$   $$$$ $$$$   $$   $$$
+    $$    $$   $$$$$$$   $$    $$
+     $$    $    $$$$$    $    $$
+      $     $           $     $
+       $     $         $    $
+◈•─────•◈•@ShuBhamg0sain•◈•─────•◈•
+'''
+
+
+logo_design_4 = Fore.RED + '''
+•◈•───────────────────•◈•@ShuBhamg0sain•◈•──────────────────•◈•
+    .o oOOOOOOOo                                            OOOo
+    Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO
+    OboO"""""""""""".OOo. .oOOOOOo.    OOOo.oOOOOOo.."""""""""'OO
+    OOP.oOOOOOOOOOOO "POOOOOOOOOOOo.   `"OOOOOOOOOP,OOOOOOOOOOOB'
+    `O'OOOO'     `OOOOo"OOOOOOOOOOO` .adOOOOOOOOO"oOOO'    `OOOOo
+    .OOOO'            `OOOOOOOOOOOOOOOOOOOOOOOOOO'            `OO
+    OOOOO   GHOSTSPLOIT   '"OOOOOOOOOOOOOOOO"`  GHOSTSPLOIT   oOO
+   oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.
+  oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO
+ OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"`  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO
+ "OOOO"       "YOoOOOOMOIONODOO"`  .   '"OOROAOPOEOOOoOY"     "OOO"
+    Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :`
+    :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?         .
+    .            oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo
+                 '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':
+                      `$"  `OOOO' `O"Y ' `OOOO'  o             .
+    .                  .     OP"          : o     .
+•◈•───────────────────•◈•@ShuBhamg0sain•◈•──────────────────•◈•
+'''
+
+
+logo_design_3 = Fore.RED + '''
+•◈•─────•◈•@ShuBhamg0sain•◈•─────•◈•
+                      ,____
+                      |---.\\
+              ___     |    `
+             / .-\  ./=)
+            |  |"|_/\/|
+            ;  |-;| /_|           
+           / \_| |/ \ |
+          /      \/\( |
+          |   /  |` ) |  GHOSTSPLOIT
+          /   \ _/    |
+         /--._/  \    |
+         `/|)    |    /
+           /     |   |
+         .'      |   |
+        /         \  |
+       (_.-.__.__./  /
+•◈•─────•◈•@ShuBhamg0sain•◈•─────•◈•
+'''
+
+
+logo_design_2 = Fore.RED + '''
+•◈•────────────•◈•@ShuBhamg0sain•◈•────────────•◈•
+                      :::!~!!!!!:.
+                  .xUHWH!! !!?M88WHX:.
+                .X*#M@$!!  !X!M$$$$$$WWx:.
+               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+             ~!~!!!! .: BLACK-KILLER$$$$RMM!
+               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+             :X- M$$$$       `"T#$T~!8$WUXU~
+            :%`  ~#$$$m:        ~!~ ?$$$$$$
+          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+.....   -~~\@#$_&-+():<` !    ~?T#$$@@W@*?$$      /`
+W$@@M!!! .!~~ \@#$_&-+()!!     .:XUW$W!~ `"~:    :
+#"~~`.:x%`!!  \@#$_&-+()!H:   !WM$$$$Ti.: .!WUn+!`
+:::~:!!`:X~ .:\@#$_&-+() ?H.!u "$$$B$$$!W:U!T$$M~
+.~~   :X@!.-~   \?@WTWo("*$$$W$TH$! `
+Wi.~!X$?!-~    : \@#$_&-+()?$$$B$Wu("**$RM!    GHOSTSPLOIT
+$R@i.~~ !     :   \@#$_&-+()~$$$$$B$$en:``
+?MXT@Wx.~    :     \@#$_&-+()~"##*$$$$M~ 
+•◈•────────────•◈•@ShuBhamg0sain•◈•────────────•◈•
+'''
