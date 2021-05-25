818ef1fb1c2f066582291c6f759bc40418f20d20
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-25 09:50:42 +0530

    Create termuxsetup.sh

---
+++ termuxsetup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo $red 
+echo ""----------------------------------------------------------------------------- ""
+echo " This Script is ShuBhamg0sain by now start"
+echo "---------------------------------------------------------------------------------"
+echo "."
+echo  "#####################################"                                                                   
+echo   "####################################"
+echo "."
+
+
+clear
+apt install python -y  
+clear         
+#installation......adb
+apt install wget && wget https://raw.githubusercontent.com/ShuBhamg0sain/adb-termux/main/install.sh && bash install.sh
+cd GHOSTSPLOIT
+clear
+echo   " processing wait......"
+cd install
+python2 termux.py
+echo  "#####################################"                                                                 
+echo   "####################################"
+
+
+echo  "#####################################"       
+echo   " follow me https://instagram.com/shubham_g0sain"                                                                   echo   "####################################                                                                 
+echo   "####################################"            
+echo   " processing wait......"                                                                    
+echo   "####################################"
+echo " If You Connected To Internet Then u downloaded success fully follow me Instagram https://www.instagram.com/shubham_g0sain/?hl=en "
+echo " ??? now rock and hack ??? "
+exit
+
+
+
+
